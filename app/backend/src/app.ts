07f46908266adfafa3592dd5d8c94ad8da49b75a
Erik Yamamoto <erik.ymmt@gmail.com>
2022-11-22 20:12:40 -0300

    feat: Add router

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import router from './routers';
 
 class App {
   public app: express.Express;
@@ -22,6 +23,7 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+    this.app.use(router);
   }
 
   public start(PORT: string | number):void {
