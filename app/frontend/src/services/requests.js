96cebf2a59e711bf68b95ffec2e9989654bc9fbb
Erik Yamamoto <erik.ymmt@gmail.com>
2022-11-30 19:41:42 -0300

    build: Adjust api base endpoint for deploy

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+  baseURL: 'https://tfc-backend-production.up.railway.app/',
 });
 
 export const setToken = (token) => {
